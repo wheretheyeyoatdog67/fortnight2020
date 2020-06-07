50e170c7017160fa053c05076755c4437ff9fd35
wheretheyeyoatdog67 <ec322@njit.edu>
2020-06-07 00:39:15 -0400

    Refine

--- devUI.js
+++ devUI.js
@@ -49,7 +49,7 @@ function cellUI(){
 }
 function specBarUI(){
   stroke(255);
-  fill(255- 2.5*specBar,0,255-2.5*specBar);
+  fill(255- 2.5*specBar,0,0);
   rect(260,90,20,1.3*specBar);
   noStroke();
 }
